3897ad60f2604cb4201e9db06e1d97ca3ce50afb
Christos Tsolakis <ctsolakis@cs.odu.edu>
2018-01-21 11:52:52 -0500

    add support for both versions 3 and 4

--- vtkMEDITReader.cxx
+++ vtkMEDITReader.cxx
@@ -8,6 +8,7 @@
 
 #include "libmeshb7.h"
 
+
 vtkStandardNewMacro(vtkMEDITReader);
 
 
@@ -42,17 +43,30 @@ vtkMEDITReader::~vtkMEDITReader()
     }
 
     // open a MEDIT file for reading
-    long long InpMsh;
-    int ver,dim;
-    InpMsh = GmfOpenMesh(this->FileName, GmfRead, &ver, &dim);
+    int64_t InpMsh;
+    int version,dim;
+    InpMsh = GmfOpenMesh(this->FileName, GmfRead, &version, &dim);
     if(InpMsh == 0)
     {
         vtkWarningMacro(<<"Could not open MEDIT file");
         return 0;
     }
+    if( dim != 3)
+    {
+        vtkErrorMacro(<<"Unsupported mesh dimension " << dim );
+        return 0;
+    }
+    if(version != 3 and version !=4)
+    {
+        vtkErrorMacro(<<"Unsupported mesh version " << dim );
+        return 0;
+    }
+    
+
     vtkDebugMacro(<<"File Opened");
     // Read the number of vertices
-    int NmbVer = GmfStatKwd(InpMsh, GmfVertices);
+    int64_t NmbVer = GmfStatKwd(InpMsh, GmfVertices);
+
     // Set up point container
     vtkSmartPointer<vtkPoints> pts = vtkSmartPointer<vtkPoints>::New();
     pts->SetDataTypeToDouble();
@@ -63,7 +77,7 @@ vtkMEDITReader::~vtkMEDITReader()
     GmfGotoKwd(InpMsh, GmfVertices);
     double p[3];
     int rt; //reference , not in use in this module
-    for(int i=0;i<NmbVer;i++)
+    for(int64_t i=0;i<NmbVer;i++)
     {
         GmfGetLin(  InpMsh, GmfVertices, &p[0], &p[1], &p[2],&rt);
         //vtkDebugMacro(<<p[0] << " " <<p[1] << " " << p[2]);
@@ -73,21 +87,36 @@ vtkMEDITReader::~vtkMEDITReader()
     vtkDebugMacro(<<"DONE");
 
     // Get Number of Triangles
-    int NmbTri = GmfStatKwd(InpMsh, GmfTriangles);
+    int64_t NmbTri = GmfStatKwd(InpMsh, GmfTriangles);
     vtkDebugMacro(<<NmbTri);
     // Read triangles
     GmfGotoKwd(InpMsh, GmfTriangles);
     vtkSmartPointer<vtkCellArray> polys = vtkSmartPointer<vtkCellArray>::New();
     vtkIdType vtkVerts[3]; // connectivity information of a single triangle
-    int verts[3];
-    for (int i=0; i < NmbTri; i++)
+    for (int64_t i=0; i < NmbTri; i++)
     {
-        GmfGetLin(  InpMsh, GmfTriangles, &verts[0], &verts[1],&verts[2],&rt);
+        if(version == 3)
+        {
+            int verts[3];
+            GmfGetLin(  InpMsh, GmfTriangles, &verts[0], &verts[1],&verts[2],&rt);
+            //vtkDebugMacro(<<verts[0] << " " <<verts[1] << " " << verts[2]);
+            vtkVerts[0] = verts[0];
+            vtkVerts[1] = verts[1];
+            vtkVerts[2] = verts[2];
+        }
+        else
+        {
+            int64_t verts[3];
+            GmfGetLin(  InpMsh, GmfTriangles, &verts[0], &verts[1],&verts[2],&rt);
+            //vtkDebugMacro(<<verts[0] << " " <<verts[1] << " " << verts[2]);
+            vtkVerts[0] = verts[0];
+            vtkVerts[1] = verts[1];
+            vtkVerts[2] = verts[2];
+        }
         // mesh/meshb are one based
-        //vtkDebugMacro(<<verts[0] << " " <<verts[1] << " " << verts[2]);
-        vtkVerts[0] = verts[0] -1;
-        vtkVerts[1] = verts[1] -1;
-        vtkVerts[2] = verts[2] -1;
+        --vtkVerts[0];
+        --vtkVerts[1];
+        --vtkVerts[2];
         //vtkDebugMacro(<<vtkVerts[0] << " " <<vtkVerts[1] << " " << vtkVerts[2]);
         polys->InsertNextCell(3,vtkVerts);
     }
