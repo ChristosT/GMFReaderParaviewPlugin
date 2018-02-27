17fd8360eafbc7164f6e068ac94c0d9f2af2ec80
Christos Tsolakis <ctsolakis@cs.odu.edu>
2018-02-27 16:14:42 -0500

    save reference information inside the vtk mesh

--- ConnectivityReader.h
+++ ConnectivityReader.h
@@ -4,6 +4,8 @@
 #include "libmeshb7.h"
 #include "vtkUnstructuredGrid.h"
 #include "vtkCellType.h"
+#include "vtkCellData.h"
+#include <vtkTypeInt64Array.h>
 
 template<typename IndexType>
 void ReadConnectivity(int64_t& InpMsh,vtkUnstructuredGrid* output)
@@ -13,6 +15,11 @@ void ReadConnectivity(int64_t& InpMsh,vtkUnstructuredGrid* output)
     int64_t NmbTet   = GmfStatKwd(InpMsh, GmfTetrahedra);
     output->Allocate(NmbTri + NmbTet);
 
+    vtkSmartPointer<vtkTypeInt64Array> reference = vtkSmartPointer<vtkTypeInt64Array>::New();
+    reference->SetNumberOfComponents(1);
+    reference->SetNumberOfTuples(NmbTri + NmbTet);
+    reference->SetName("ref");
+    
     IndexType v0,v1,v2,v3,rt;
     
     GmfGotoKwd(InpMsh, GmfTriangles);
@@ -26,10 +33,11 @@ void ReadConnectivity(int64_t& InpMsh,vtkUnstructuredGrid* output)
         tria[1] = v1-1;
         tria[2] = v2-1;
         output->InsertNextCell(VTK_TRIANGLE,3,tria);
+        reference->InsertTuple1(i,rt);
     }
-    
     GmfGotoKwd(InpMsh, GmfTetrahedra);
     vtkIdType tet[4];
+
     for (int64_t i=0; i < NmbTet; i++)
     {
         GmfGetLin(  InpMsh, GmfTetrahedra, &v0, &v1,&v2,&v3,&rt);
@@ -40,6 +48,8 @@ void ReadConnectivity(int64_t& InpMsh,vtkUnstructuredGrid* output)
         tet[2] = v2-1;
         tet[3] = v3-1;
         output->InsertNextCell(VTK_TETRA,4,tet);
+        reference->InsertTuple1(NmbTri + i,rt);
     }
+    output->GetCellData()->AddArray(reference);
 }
 #endif
